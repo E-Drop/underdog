1158477ebf0753dad4a50051fab4d05ba95413ba
Axel Garcia <axel.garcia94@gmail.com>
2018-09-01 10:50:21 +0200

    build Splash

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 'use strict';
 
-function buildDOM(html) {
+function buildDom(html) {
   var div = document.createElement('div');
   div.innerHTML = html;
   return div.children[0];
 }
+
+var splashMain;
+
+function main () {
+
+//-----SPLASH------//
+
+  function buildSplash() {
+    splashMain = buildDom(
+     `<main>
+       <h1>Underdog</h1>
+       <button>Start</button>
+     </main>`
+   );
+ 
+   document.body.appendChild(splashMain);  
+  //  var buttonStart = splashMain.querySelector('button');
+
+  }
+
+//------GAME------//
+
+
+  buildSplash();
+} //---End of main---//
+
+window.addEventListener('load', main);
